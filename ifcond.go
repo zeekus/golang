3d3434b870451796ba58e77d96e5c21674c8b4fb
Zeekus <ipmasqman@gmail.com>
2022-12-22 14:10:58 -0500

    retesting

--- ifcond.go
+++ ifcond.go
@@ -1,15 +1,15 @@
 package main
 
-import "fmt"
+import ("fmt")
 
 func main() {
-
-	#loop 10 - 0
+	//count backwards 10 to 1
 	for i := 10; i > 0; i-- {
-          #check if we are a factor of 2
+          //check if we are a factor of 2
           if i %2 == 0 {
 	    str := fmt.Sprintf("i:%d is a factor of 2",i)
 	    fmt.Println(str)
+	  //check if we are not a factor of 2
           } else {
 	    str := fmt.Sprintf("i:%d is not a factor of 2",i)
 	    fmt.Println(str)
